039ee95a714af7fb05747b37cd365f7996b72c5a
qwe1 <qwe1@qwe.com>
2025-03-13 01:19:22 +0200

    Tag selected and unselected are visually populated on the page.

--- static/js/index.js
+++ static/js/index.js
@@ -32,6 +32,11 @@ itemListTemplate = `
 `;
 
 const NO_TAG_NAME_ID = "no_tag";
+const NO_TAG_TAG = {
+  tag: NO_TAG_NAME_ID,
+  uuid: NO_TAG_NAME_ID,
+  description: NO_TAG_NAME_ID,
+};
 
 function fileIsImage(fName) {
   let last_dot_idx = fName.lastIndexOf(".");
@@ -53,39 +58,21 @@ function findImageFileOfItem(item) {
 }
 
 async function tag_click(elem) {
-  console.log("Have tag click. This :>> ", this);
-  console.log("elem :>> ", elem);
-  console.log("elem.id :>> ", elem.id);
+  // console.log("Have tag click. This :>> ", this);
+  // console.log("elem :>> ", elem);
+  // console.log("tag_click elem.id :>> ", elem.id);
   let filtering_tag_uuid = elem.id;
 
-  // let fetchURL = `${BASE_URL}item/all`;
-  // let jsonObj = await fetchJSON2(fetchURL, null);
-  // console_debug("index:60 jsonObj:>>", jsonObj);
+  GLOBAL_STATE.items_selection_criteria.tags_selected.push(filtering_tag_uuid);
+  // console_debug(
+  //   "index:62 tag_click GLOBAL_STATE.items_selection_criteria.tags_selected::",
+  //   GLOBAL_STATE.items_selection_criteria.tags_selected
+  // );
+  populate_tags();
 
   let jsonObj = await get_items_from_server(0, filtering_tag_uuid);
   // console_debug("index:56 item_list:>>", jsonObj);
   reload_page(jsonObj);
-
-  // let filtered_items = jsonObj.items.filter((item) => {
-  //   // console.log("item :>> ", item);
-  //   if (filtering_tag_uuid == "no_tag" && item.tags.length == 0) return true;
-  //   if (item.tags.length == 0) return false;
-  //   console_debug("index:66 item:>>", item);
-  //   let filtered_tags = item.tags.filter((tag) => {
-  //     // console.log("tag.uuid :>> ", tag.uuid);
-  //     // console_debug("index:69 filtering_tag_uuid:>>", filtering_tag_uuid);
-  //     // console_debug(
-  //     //   "index:70 tag.uuid==filtering_tag_uuid:>>",
-  //     //   tag.uuid == filtering_tag_uuid
-  //     // );
-  //     return tag.uuid == filtering_tag_uuid ? true : false;
-  //   }); //let filtered_tags = item.tags.filter((tag)=>{
-  //   // console.log("filtered_tags :>> ", filtered_tags);
-  //   if (filtered_tags.length == 0) return false;
-  //   return true;
-  // }); //let filtered_items = jsonObj.map((item)=>{
-  // // console.log("filtered_items :>> ", filtered_items);
-  // populate_items_list(filtered_items);
 } //async function tag_click(elem) {
 
 function populate_items_list(items) {
@@ -108,21 +95,65 @@ function populate_items_list(items) {
   document.getElementById("items_placeholder").innerHTML = itemsHTML;
 }
 
-function populate_tags(tags, add_no_tag = true) {
+function remove_tag_from_selected(elem) {
+  console_debug("index:99 elem::", elem);
+  console_debug("index:100 elem.dataset.taguuid::", elem.dataset.taguuid);
+  console_debug(
+    "index:100 GLOBAL_STATE.items_selection_criteria.tags_selected::",
+    GLOBAL_STATE.items_selection_criteria.tags_selected
+  );
+  let idx_to_remove =
+    GLOBAL_STATE.items_selection_criteria.tags_selected.indexOf(
+      elem.dataset.taguuid
+    );
+  console_debug("index:108 idx_to_remove::", idx_to_remove);
+
+  GLOBAL_STATE.items_selection_criteria.tags_selected.splice(idx_to_remove, 1);
+  console_debug(
+    "index:100 GLOBAL_STATE.items_selection_criteria.tags_selected::",
+    GLOBAL_STATE.items_selection_criteria.tags_selected
+  );
+
+  populate_tags();
+}
+
+function populate_tags(visible_tag_filter = "") {
+  // console_debug("index:94 populate_tags::", "");
+  // console_debug("index:95 visible_tag_filter::", visible_tag_filter);
+  // console_debug(
+  //   "index:96 GLOBAL_STATE.items_selection_criteria.tags_selected::",
+  //   GLOBAL_STATE.items_selection_criteria.tags_selected
+  // );
+  // TODO: move completely to global state management, or create separate object, which will manage tags selection
+  let tags = GLOBAL_STATE.items_selection_criteria.tags;
   tagsHTML = "";
-  if (add_no_tag)
-    tagsHTML += `<a href="#" id="${NO_TAG_NAME_ID}" onclick="tag_click(this)">${NO_TAG_NAME_ID}</a>; `;
-  // console_debug("index:106 tags presort:>>", tags);
-  tags.sort((a, b) => {
-    return a.tag.localeCompare(b.tag);
-  });
-  // console_debug("index:107 tags after sort:>>", tags);
+  tagsSelectedHTML = "";
+  // if (add_no_tag)
+  //   tagsHTML += `<a href="#" id="${NO_TAG_NAME_ID}" onclick="tag_click(this)">${NO_TAG_NAME_ID}</a>; `;
+
   for (tagIdx in tags) {
     let tag = tags[tagIdx];
-    tagsHTML += `<a href="#" id="${tag.uuid}" onclick="tag_click(this)">${tag.tag}</a>; `;
+    // console_debug("index:109 tag::", tag);
+    if (
+      GLOBAL_STATE.items_selection_criteria.tags_selected.includes(tag.uuid)
+    ) {
+      console.log("Adding to selected :>> ");
+      tagsSelectedHTML += `<div class="row"><div class="col">${tag.tag}..<a href="#" data-taguuid="${tag.uuid}" onclick="remove_tag_from_selected(this)">[x]</a></div></div>`;
+    } else {
+      if (
+        visible_tag_filter != "" &&
+        !tag.tag.toLowerCase().includes(visible_tag_filter)
+      )
+        continue;
+      tagsHTML += `<a href="#" id="${tag.uuid}" onclick="tag_click(this)">${tag.tag}</a>; `;
+    }
   }
+  document.getElementById("selected-tags-placeholder").innerHTML =
+    tagsSelectedHTML;
+  // console_debug("index:118 tagsSelectedHTML::", tagsSelectedHTML);
   document.getElementById("tags-holder").innerHTML = tagsHTML;
-}
+  // console_debug("index:128 tagsHTML::", tagsHTML);
+} //function populate_tags(tags, add_no_tag = true) {
 
 async function reload_page(server_json) {
   populate_items_list(server_json.items);
@@ -130,8 +161,6 @@ async function reload_page(server_json) {
 }
 
 async function next_page(el) {
-  // console_debug("index:126 el::", el);
-  // console_debug("index:125 el.dataset.pageno:>>", el.dataset.pageno);
   let served_json = await get_items_from_server(el.dataset.pageno);
   await reload_page(served_json);
 }
@@ -175,31 +204,44 @@ async function tag_search() {
     .value.toLowerCase();
   console_debug("index:102 search_term:>>", search_term);
 
-  let tagsURL = `${BASE_URL}tag`;
-  let tagsResp = await fetchJSON2(tagsURL, null);
-  // console_debug("index:74 tagsResp:>>", tagsResp);
-  let tags_filtered = tagsResp.tags.filter((tag) => {
-    return tag.tag.toLowerCase().includes(search_term);
-  });
+  // let tagsURL = `${BASE_URL}tag`;
+  // let tagsResp = await fetchJSON2(tagsURL, null);
+  // // console_debug("index:74 tagsResp:>>", tagsResp);
+  // let tags_filtered = tagsResp.tags.filter((tag) => {
+  //   return tag.tag.toLowerCase().includes(search_term);
+  // });
 
-  let include_no_tag = NO_TAG_NAME_ID.toLowerCase().includes(search_term)
-    ? true
-    : false;
-  populate_tags(tags_filtered, include_no_tag);
+  // let include_no_tag = NO_TAG_NAME_ID.toLowerCase().includes(search_term)
+  //   ? true
+  //   : false;
+  populate_tags(search_term);
 } //async function tag_search() {
 
 window.onload = async () => {
+  GLOBAL_STATE.items_selection_criteria = {
+    tags: [NO_TAG_TAG],
+    tags_selected: [],
+    page: 0,
+    containing_words: [],
+  };
+
   let jsonObj = await get_items_from_server(0);
-  // console_debug("index:56 item_list:>>", jsonObj);
   reload_page(jsonObj);
-  // populate_items_list(jsonObj.items);
-  // generate_page_numbers(jsonObj);
 
   //Populating tags
   let tagsURL = `${BASE_URL}tag`;
   let tagsResp = await fetchJSON2(tagsURL, null);
-  // console_debug("index:74 tagsResp:>>", tagsResp);
-  populate_tags(tagsResp.tags);
+  GLOBAL_STATE.items_selection_criteria.tags.push(...tagsResp.tags);
+
+  GLOBAL_STATE.items_selection_criteria.tags.sort((a, b) => {
+    return a.tag.localeCompare(b.tag);
+  });
+
+  console_debug(
+    "index:185 GLOBAL_STATE.items_selection_criteria.tags::",
+    GLOBAL_STATE.items_selection_criteria.tags
+  );
+  populate_tags();
 
   document
     .getElementById("tag_search_input")
