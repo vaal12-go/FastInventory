1bdf3e72e0d9a6ea0f71966423a29cb398829a0e
qwe1 <qwe1@qwe.com>
2025-04-18 20:08:36 +0300

    InventoryItem component works

---
+++ react-front/src/components/index/InventoryItem.jsx
@@ -0,0 +1,27 @@
+
+
+export function InventoryItem({ item }) {
+
+    return (
+        <div>
+            {/* <!-- ITEM --> */}
+            <div className="card inventory-card me-1 mb-1">
+                <h6 className="card-header"><a href="#">{item.name}</a></h6>
+                <div className="card-body">
+                    <img src="img/toolbox.jpg" alt="" className="inventory-card-img border" />
+                    <p className="card-title inventory-card-title">
+                        {item.description}</p>
+                    <div className="rounded-1 inventory-card-tag"> book</div>
+                    <div className="rounded-1 inventory-card-tag">test_auto_added_item</div>
+
+                    <p className="card-text inventory-card-body">
+                        UUID: <span
+                            className="inventory-card-uuid">{item.uuid}</span>
+                    </p>
+
+                </div>
+            </div>
+            {/* <!-- ITEM END --> */}
+        </div>
+    )
+}
--- react-front/src/components/index/InventoryItems.jsx
+++ react-front/src/components/index/InventoryItems.jsx
@@ -1,4 +1,7 @@
 import useSWR from 'swr'
+import { InventoryItem } from './InventoryItem';
+import './tags.css'
+import './card.css'
 
 async function fetchInventoryItems() {
     console.log('Fetching :>> ');
@@ -6,18 +9,42 @@ async function fetchInventoryItems() {
     const itemsRes = await fetch(url)
     const itemsJSON = await itemsRes.json()
     console.log('itemsRes :>> ', itemsJSON);
-    return "HEllo"
+    return itemsJSON
 }
 
 export function InventoryItems() {
-
+    console.log('InventoryItems :>> ');
     const { data, error, isLoading } = useSWR('/api/user', fetchInventoryItems)
 
+    if (error) {
+        console.error('error :>> ', { err: error });
+        return (
+            <>
+                {/* <div>failed to load: {error.message} </div>
+        <div>{error.stack} </div> */}
+            </>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <div>loading...</div>
+        )
+    }
+
+    console.log('data: :>> ', data);
     return (
         <>
-            Inventory items list
+            {/* Inventory items list: {JSON.stringify(data)} */}
+            {
+                data.items.map((itm)=>{
+                    console.log("drawing item:", itm)
+                    return (
+                        <InventoryItem item={itm}/>
+                    )
+                })
+            }
         </>
     )
-
-}
+}//export function InventoryItems() {
 
